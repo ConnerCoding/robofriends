06329d6d9edbd1a2cf37bcbeddcf30687e55257d
Conner Schiller <conner.schiller@gmail.com>
2020-07-30 22:01:04 -0700

    added comments to card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Card = ({ name, email, id }) => {
     return (
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            {/* utilize the https://robohash.org/ API to create robots with id as the randomizer */}
             <img src={`https://robohash.org/${id}?size=200x200`} alt='robot'/>
             <div>
                 <h2>{name}</h2>
