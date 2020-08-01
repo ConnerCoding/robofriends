9f9488b1e282e9e828f1a759792dd14baf36fd95
Conner Schiller <conner.schiller@gmail.com>
2020-07-31 18:03:18 -0700

    added comment for cardlist.js

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
+    // Create the card in card.js sending the info needed, then return the component
     const cardComponent = robots.map((user, i) => {
         return (
             <Card 
