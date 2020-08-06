6af5bb03cc35e544936667b605953a7d68489dfe
Conner Schiller <conner.schiller@gmail.com>
2020-08-05 17:42:09 -0700

    added comments to scroll.js

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+// Add scroll functionality to the robot card list
+// Simply add the robots (props.children) into a div with scroll enabled
 const Scroll = (props) => {
     return (
         <div style={{overflowY: 'scroll', border: '5px solid black', height: '800px'}}>
