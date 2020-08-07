a2fd6cd1d35b45d6714b75f7e3cc95943145f6af
Conner Schiller <conner.schiller@gmail.com>
2020-08-06 20:46:49 -0700

    added comments to searchbox.js

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
+// bring in searchChange from mapDispatchToProps
+// onChange input filter the robots responsively
 const SearchBox = ({ searchChange }) => {
     return (
         <div className='pa2'>
             <form className="example">
                 <input 
-                className='pa3 ba b--green bg-lightest-blue' 
-                type="search" 
-                placeholder="search robots" 
-                name="search"
-                onChange={searchChange}
+                    className='pa3 ba b--green bg-lightest-blue' 
+                    type="search" 
+                    placeholder="search robots" 
+                    name="search"
+                    onChange={searchChange}
                 />
             </form>
         </div>
