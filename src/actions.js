0eb610be2a9c29fc146eb303b7145a67289357f3
Conner Schiller <conner.schiller@gmail.com>
2020-08-06 20:49:30 -0700

    added comments to actions.js

--- src/actions.js
+++ src/actions.js
@@ -5,11 +5,14 @@ import {
     REQUEST_ROBOTS_FAILED 
 } from './constants.js';
 
+// used for live updating of the searchbox.js
 export const setSearchField = (text) => ({
     type: CHANGE_SEARCH_FIELD,
     payload: text
 })
 
+// used to retrieve the robots from the api
+// which happens on componentDidMount
 export const requestRobots = () => (dispatch) => {
     dispatch({ type: REQUEST_ROBOTS_PENDING });
     fetch('https://jsonplaceholder.typicode.com/users')
